2ec1e692c34926f228e30066ca865b5a7ca88f79
Tom Mast <thomasomast@gmail.com>
2014-10-26 16:29:19 -0500

    Added check for mo-bile

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
 $(document).ready(function() {
-    // mobile
-    //// click to expand menu
-    //$('#menu-container #menu').click(function(event) {
-    //    event.stopPropagation();
-    //    $("#menu").addClass('expanded');
-    //});
 
-    //// click to close menu
-    //$('#menu-container').click(function() {
-    //    $("#menu").removeClass('expanded');
-    //});
+    // check for mobile
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        // click to expand menu
+        $('#menu-container #menu').click(function(event) {
+            event.stopPropagation();
+            $("#menu").addClass('expanded');
+        });
 
-    // hover on menu
-    $('#menu-container #menu').hover(
-        // on hover
-        function() {
-            $(this).addClass('expanded');
-        },
-        // on hover leave
-        function() {
-            $(this).removeClass('expanded');
-        }
-    );
+        // click to close menu
+        $('#menu-container').click(function() {
+            $("#menu").removeClass('expanded');
+        });
+    } else { 
+        // hover on menu
+        $('#menu-container #menu').hover(
+            // on hover
+            function() {
+                $(this).addClass('expanded');
+            },
+            // on hover leave
+            function() {
+                $(this).removeClass('expanded');
+            }
+        );
+    }
 
 });
